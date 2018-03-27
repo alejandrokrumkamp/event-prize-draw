5e1e3d2be75358b1407ddd6729155226148ce447
Alejandro Krumkamp <alekd9@gmail.com>
2018-03-27 15:10:36 -0300

    Adding fetched attendees to an array

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const request = require('request');
 const fs = require('fs');
 const configFile = JSON.parse(fs.readFileSync('auth.cfg', 'utf8'));
+let token = configFile.bearer;
+let bringMoreItems = true;
 
-request.get(configFile.urlAPI, {
-    'auth': {
-        'bearer': configFile.bearer
-    }
-
-}).on('response', function(response) {
-    console.log(response.statusCode) // 200
-}).on('data', function(data) {
-    let parsedData = JSON.parse(data);
-    let attendees = parsedData.attendees;
-    attendees.forEach(attendee => {
-        console.log(attendee.profile.first_name);
-    console.log(attendee.profile.last_name);
-    })
-});
+function fetchAttendees(){
+    let result = [];
+    request.get(configFile.urlAPI
+        ,{
+        'auth': {
+            'bearer': token
+        }
+    }).on('response', function(response) {
+        console.log(response.statusCode) // 200
+    }).on('data', function(data) {
+        let parsedData = JSON.parse(data);
+        console.log(parsedData.pagination);
+        bringMoreItems = parsedData.pagination.has_more_items;
+        let attendees = parsedData.attendees;
+        attendees.forEach(attendee => {
+            result.push(attendee);
+        });
+        return result;
+    });
+}
