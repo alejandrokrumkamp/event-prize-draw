323a3fbba583187820ece23e1226053de4fb966f
Alejandro Krumkamp <alekd9@gmail.com>
2018-03-30 21:39:44 -0300

    successfully returning attendees from API with fetch

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-const request = require('request');
+const fetch = require('isomorphic-fetch');
 const fs = require('fs');
 const configFile = JSON.parse(fs.readFileSync('auth.cfg', 'utf8'));
-let token = configFile.bearer;
-let bringMoreItems = true;
 
-function fetchAttendees(){
-    let result = [];
-    request.get(configFile.urlAPI
-        ,{
-        'auth': {
-            'bearer': token
-        }
-    }).on('response', function(response) {
-        console.log(response.statusCode) // 200
-    }).on('data', function(data) {
-        let parsedData = JSON.parse(data);
-        console.log(parsedData.pagination);
-        bringMoreItems = parsedData.pagination.has_more_items;
-        let attendees = parsedData.attendees;
-        attendees.forEach(attendee => {
-            result.push(attendee);
-        });
-        return result;
-    });
-}
+const fetchAttendees = ()=> {
+    //let bringMoreItems = true;
+    return fetch(configFile.urlAPI,configFile.settings)
+        .then( response => {return response.json()})
+        .then( data => {
+            console.log(data.pagination);
+            bringMoreItems = data.pagination.has_more_items;
+            let attendees = data.attendees;
+            attendees.forEach(attendee => {
+                result.push(attendee);
+            });
+            return attendees;
+        })
+        .catch( error => console.log(error))
+};
+
+fetchAttendees()
+    .then((attendees)=>console.log(attendees));
