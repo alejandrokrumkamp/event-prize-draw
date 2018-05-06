50dc3eecb7a10290b3741c16e341fac7d5037e1e
Alejandro Krumkamp <alekd9@gmail.com>
2018-05-06 20:06:09 -0300

    Added sticky footer for sponsors

--- public/js/index.js
+++ public/js/index.js
@@ -3,4 +3,5 @@ const getRandomAttendee = ()=>{
         .then(response => response.json())
         .then(data => data.randomAttendee)
         .then(randomAttendee => alert("Congratulations "+randomAttendee+"! You won!"))
+        .catch(console.log("An error ocurred while fetching the random attendee"))
 }
